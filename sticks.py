8c34c77ae38438b64486a5378d44768c2985f6ab
Jonathan Frederick <jrfreder@gmail.com>
2015-09-08 16:51:49 -0400

    consolidated with get_int

--- sticks.py
+++ sticks.py
@@ -4,33 +4,33 @@ AI players can play against each other.
 In the Game of Sticks there is a heap of sticks on a board.
 On their turn, each player picks up 1 to 3 sticks. The one
 who has to pick the final stick will be the loser."""
+def get_int(low, high, prompt):
+    """Prompts the player for an integer within a range"""
+    while True:
+        try:
+            integer = int(input(prompt))
+            if integer < low or integer > high:
+                integer/0
+            break
+        except:
+            print("Please enter an integer between", low, "and", high)
+            continue
+    return integer
+
 
 def prompt_count():
     """Prompts the player for the number of sticks to begin the game"""
-    while True:
-        try:
-            count = int(input("How many sticks are there on the table initially (10-100)?  "))
-            if count < 10 or count > 100:
-                count/0
-            break
-        except:
-            print("Please enter an integer between 10 and 100")
-            continue
-    return count
+    return get_int(10, 100, "How many sticks are there on the table initially (10-100)?  ")
 
 
 def prompt_human_count():
     """Prompts the user for the number of human players (0-2)"""
-    while True:
-        try:
-            humans = int(input("Please input the number of human players (0-2)"))
-            if humans < 0 or humans > 2:
-                humans/0
-            break
-        except:
-            print("Please enter an integer between 0 and 2")
-            continue
-    return humans
+    return get_int(0, 2, "Please input the number of human players (0-2)")
+
+
+def prompt_choice(player):
+    return get_int(1, 2, player+": How many sticks do you take (1-3)?  ")
+
 
 def get_players(humans):
     """Prompts for player names or returns a dictionary for an AI"""
@@ -47,19 +47,21 @@ def get_players(humans):
 
 def turn(count, player):
     """Prompts the player for the number of sticks to pick up"""
-    pass
+    choice = prompt_choice(player)
+    return count, player()
 
 
 def main():
     print("Welcome to the Game of Sticks!")
     count = prompt_count()
     turn_alt = 1
-    print(get_players(prompt_human_count()))
-#    player1, player2 = prompt_name()
+    player_1, player_2 = get_players(prompt_human_count())
     while count < 0:
         if turn_alt > 0:
-            turn(count, player1)
-
+            turn, player1 = turn(count, player1)
+        else:
+            turn, player2 = turn(count, player2)
+        turn_alt * -1
 
 
 if __name__ == '__main__':
