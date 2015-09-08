ea0f810067e8a807bf5a24977c6d2347aff96b2a
Jonathan Frederick <jrfreder@gmail.com>
2015-09-08 19:04:15 -0400

    implimented add and subtract dict tests

--- sticks_test.py
+++ sticks_test.py
@@ -6,14 +6,19 @@ class TestSticks(unittest.TestCase):
     pass
 
 def test_reset_dict(self):
-    self.assertEqual(reset_dict({1:[1,2], 2:[2,3], 3:[1,3]},
-                            {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}))
-    self.assertEqual(reset_dict({1:[], 2:[], 3:[]}))
-                            {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}))
-    self.assertEqual(reset_dict({1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}, 
-                            {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}))
+    self.assertEqual(reset_dict({1:[1,2], 2:[2,3], 3:[1,3]}),
+                            {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]})
+    self.assertEqual(reset_dict({1:[], 2:[], 3:[]})
+                            {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]})
+    self.assertEqual(reset_dict({1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}),
+                            {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]})
 
+def test_add_dict(self):
+    self.assertEqual(add_dict([(1, 3),(2, 2),(3, 1)], {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]})
+                            {1:[1,2,3,3], 2:[1,2,3,2], 3:[1,2,3,1]})
 
-
+def test_subtract_dict(self):
+    self.assertEqual(subtract_dict([(1, 3),(2, 2),(3, 1)], {1:[1,2,3], 2:[1,2,3], 3:[1,2,3]})
+                            {1:[1,2], 2:[1,3], 3:[2,3]})
 if __name__ == '__main__':
     unittest.main()
