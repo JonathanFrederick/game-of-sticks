093e9bdf34b6a4767f66a3a2178d2d2d568124d6
Jonathan Frederick <jrfreder@gmail.com>
2015-09-08 15:31:37 -0400

    added scaffolding

--- sticks.py
+++ sticks.py
@@ -5,7 +5,8 @@ In the Game of Sticks there is a heap of sticks on a board.
 On their turn, each player picks up 1 to 3 sticks. The one
 who has to pick the final stick will be the loser."""
 
-def get_count():
+def prompt_count():
+    """Prompts the player for the number of sticks to begin the game"""
     while True:
         try:
             count = int(input("How many sticks are there on the table initially (10-100)?  "))
@@ -18,9 +19,24 @@ def get_count():
     return count
 
 
+def prompt_human_count():
+    """Prompts the user for the number of human players (0-2)"""
+    pass
+
+
+def turn(count):
+    """Prompts the player for the number of sticks to pick up"""
+    pass
+
+
 def main():
     print("Welcome to the Game of Sticks!")
-    get_count()
+    count = prompt_count()
+    turn_alt = 1
+
+#    while count < 0:
+
+
 
 if __name__ == '__main__':
     main()
