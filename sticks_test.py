77611108f5f4dbc49ef1f2df0a3c1e9a84dd8d9a
Jonathan Frederick <jrfreder@gmail.com>
2015-09-08 17:54:47 -0400

    added test_reset_dict

--- sticks_test.py
+++ sticks_test.py
@@ -1,1 +1,17 @@
 """A program to test the functions of sticks.py"""
+import unittest
+from sticks import *
+
+class TestSticks(unittest.TestCase):
+    pass
+
+def test_reset_dict(self):
+    self.assertEqual(reset_dict({"HI":{1:[1,2], 2:[2,3], 3:[1,3]}},
+                            {"HI":{1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}}))
+    self.assertEqual(reset_dict({"HI":{1:[], 2:[], 3:[]}}, ))
+                            {"HI":{1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}}))
+    self.assertEqual(reset_dict({"HI":{1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}}, 
+                            {"HI":{1:[1,2,3], 2:[1,2,3], 3:[1,2,3]}}))
+
+if __name__ == '__main__':
+    unittest.main()
